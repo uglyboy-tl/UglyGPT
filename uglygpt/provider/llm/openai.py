0733c7a5aeb91a0fc9a757082f69353b6e4510ae
uglyboy <uglyboy@uglyboy.cn>
2023-05-22 15:19:14 +0800

    增加 tiktoken ，控制 chatgpt 的字数

--- uglygpt/provider/llm/openai.py
+++ uglygpt/provider/llm/openai.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 import openai
+import tiktoken
 from dataclasses import dataclass, field
 from uglygpt.provider.llm.base import LLMProvider
 from uglygpt.base import config
@@ -16,8 +17,17 @@ class OpenAILLM(LLMProvider):
     temperature: float = 0.7
     MAX_TOKENS: int = 4096
 
-    def instruct(self, prompt: str, tokens: int=1024) -> str:
+    def _num_tokens(self, prompt: str) -> int:
+        encoding = tiktoken.encoding_for_model(self.model)
+        num_tokens = len(encoding.encode(prompt))
+        return num_tokens
+
+    def instruct(self, prompt: str, tokens: int=None) -> str:
+        if tokens is None:
+            tokens = self._num_tokens(prompt)
         max_new_tokens = int(self.MAX_TOKENS) - tokens
+        if max_new_tokens <= 0:
+            raise ValueError("Prompt is too long.")
         completions = openai.Completion.create(
             model=self.model,
             prompt=prompt,
