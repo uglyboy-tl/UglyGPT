1f64f4c06e1c90f69ba9773195a68c1e66421965
uglyboy-tl <uglyboy@aliyun.com>
2023-05-24 15:48:41 +0800

    测试贡献

--- uglygpt/chains/__init__.py
+++ uglygpt/chains/__init__.py
@@ -1,9 +1,11 @@
 from uglygpt.chains.base import Chain
 from uglygpt.chains.llm import LLMChain
 from uglygpt.chains.transform import TransformChain
+from uglygpt.chains.memory import BaseMemory
 
 __all__ = [
     "Chain",
     "LLMChain",
     "TransformChain",
+    "BaseMemory"
     ]
