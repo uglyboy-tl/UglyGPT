4eeba9b1b9c0e94899a120d4a5f10c1e2138b062
uglyboy <uglyboy@uglyboy.cn>
2023-05-24 12:45:13 +0800

    增加 SQL 工具，但未验证效果

---
+++ uglygpt/tools/sql_database/__init__.py
@@ -0,0 +1,8 @@
+from uglygpt.tools.sql_database.tool import QuerySQLDataBaseTool, InfoSQLDatabaseTool, ListSQLDatabaseTool, QueryCheckerTool
+
+__all__ = [
+    "QuerySQLDataBaseTool",
+    "InfoSQLDatabaseTool",
+    "ListSQLDatabaseTool",
+    "QueryCheckerTool",
+]
