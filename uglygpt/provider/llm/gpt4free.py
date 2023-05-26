c15b4c269e3e0f41fc3fe51da5ba951f367ee7b7
uglyboy-tl <25730357+uglyboy-tl@users.noreply.github.com>
2023-05-27 06:37:39 +0800

    python3.9还不支持 match 语句，换成 if elif

--- uglygpt/provider/llm/gpt4free.py
+++ uglygpt/provider/llm/gpt4free.py
@@ -13,32 +13,30 @@ class GPT4FreeLLM(LLMProvider):
     type: str = "useless"
 
     def instruct(self, prompt: str, tokens: int = 0) -> str:
-        match self.type:
-            case "useless":
-                response = gpt4free.Completion.create(Provider.UseLess, prompt=prompt)
-                logger.debug(str(response))
-                return response['text']
-            # 中国区无法使用
-            case "you":
-                config.start_proxy()
-                response = gpt4free.Completion.create(Provider.You,prompt="", chat=prompt)
-                logger.debug(str(response))
-                config.stop_proxy()
-                return response.text
-            # 中国区无法使用
-            case "theb":
-                config.start_proxy()
-                response = gpt4free.theb.Completion.create(prompt=prompt)
-                config.stop_proxy()
-                return "".join(response)
-            # CouldNotGetAccountException
-            case "forefront":
-                config.start_proxy()
-                token = gpt4free.forefront.Account.create(logging=True)
-                response = gpt4free.Completion.create(
-                    Provider.ForeFront, prompt=prompt, model='gpt-4', token=token
-                )
-                logger.debug(str(response))
-                config.stop_proxy()
-                return "".join(response)
-
+        if self.type == "useless":
+            response = gpt4free.Completion.create(Provider.UseLess, prompt=prompt)
+            logger.debug(str(response))
+            return response['text']
+        # 中国区无法使用
+        elif self.type == "you":
+            config.start_proxy()
+            response = gpt4free.Completion.create(Provider.You, prompt=prompt)
+            logger.debug(str(response))
+            config.stop_proxy()
+            return response['text']
+        # 中国区无法使用
+        elif self.type == "theb":
+            config.start_proxy()
+            response = gpt4free.theb.Completion.create(prompt=prompt)
+            config.stop_proxy()
+            return "".join(response)
+        # CouldNotGetAccountException
+        elif self.type == "forefront":
+            config.start_proxy()
+            token = gpt4free.forefront.Account.create(logging=True)
+            response = gpt4free.Completion.create(
+                Provider.ForeFront, prompt=prompt, model='gpt-4', token=token
+            )
+            logger.debug(str(response))
+            config.stop_proxy()
+            return "".join(response)
