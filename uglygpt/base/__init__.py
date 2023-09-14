067a1d907b669a3cc4225934a4833572d7f46b1a
uglyboy-tl <25730357+uglyboy-tl@users.noreply.github.com>
2023-09-14 12:04:44 +0800

    将项目目录置于 File 类变量下

--- uglygpt/base/__init__.py
+++ uglygpt/base/__init__.py
@@ -2,13 +2,11 @@
 #-*-coding:utf-8-*-
 
 from .config import config
-from .logs import logger
 from .singleton import singleton
 from .file import File
 
 __all__ = [
     'config',
-    'logger',
     'singleton',
     'File',
 ]
--- uglygpt/base/file.py
+++ uglygpt/base/file.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8-*-
 
-import os
-from dataclasses import dataclass, field
 from loguru import logger
 from pathlib import Path
 
@@ -17,19 +15,20 @@ def get_project_root():
             raise Exception("Project root not found.")
         current_path = parent_path
 
-WORKSPACE_ROOT = get_project_root()
+#WORKSPACE_ROOT = get_project_root()
 
-@dataclass
 class File:
+    WORKSPACE_ROOT = get_project_root()
+
     @classmethod
     def save(cls, filename, data):
-        file_path = WORKSPACE_ROOT / filename
+        file_path = cls.WORKSPACE_ROOT / filename
         file_path.parent.mkdir(parents=True, exist_ok=True)
         file_path.write_text(data)
         logger.debug(f"Saving file to {file_path}")
 
     @classmethod
     def load(cls, filename):
-        with open(WORKSPACE_ROOT / filename, "r") as f:
+        with open(cls.WORKSPACE_ROOT / filename, "r") as f:
             data = f.read()
         return data
