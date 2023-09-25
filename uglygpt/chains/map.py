fdd29738ff282d53593d5164055f23c77e540dc2
uglyboy-tl <25730357+uglyboy-tl@users.noreply.github.com>
2023-09-25 10:29:02 +0800

    map 和 reduce 都支持多批量输入

--- uglygpt/chains/map.py
+++ uglygpt/chains/map.py
@@ -13,31 +13,37 @@ from .llm import LLMChain
 @dataclass
 class MapChain(Chain):
     chain: Chain = field(default_factory=LLMChain)
-    mapping_key: str = "input"
+    map_keys: List[str] = field(default_factory=lambda: ["input"])
 
     @property
     def input_keys(self) -> List[str]:
         return self.chain.input_keys
 
     def _validate_inputs(self, inputs: Dict[str, Any]) -> None:
-        if self.mapping_key not in self.input_keys:
-            raise ValueError(
-                f"MapChain expects {self.mapping_key} to be in input_keys")
+        self.num = len(inputs[self.map_keys[0]])
+        for map_key in self.map_keys:
+            assert map_key in self.input_keys, f"MapChain expects {map_key} to be in input_keys"
+            assert isinstance(inputs[map_key], List), f"MapChain expects {map_key} to be a list of strings"
+            assert len(inputs[map_key]) == self.num, f"MapChain expects {map_key} to be a list of strings with the same length"
         super()._validate_inputs(inputs)
-        if not isinstance(inputs[self.mapping_key], List):
-            raise ValueError(
-                f"MapChain expects {self.mapping_key} to be a list of strings")
 
     def _call(self, inputs: Dict[str, Any]) -> str:
-        text_dicts = [{"index": i, "text": str(item)} for i, item in enumerate(inputs[self.mapping_key])]
+        inputs_list = []
+        for i in range(self.num):
+            input = {}
+            for map_key in self.map_keys:
+                input[map_key] = inputs[map_key][i]
+            input["index"] = i
+            inputs_list.append(input)
 
         pool = Pool()
-        def map_func(text_dict):
-            new_input: Dict = {k: v for k, v in inputs.items() if k != self.mapping_key}
-            new_input[self.mapping_key] = text_dict["text"]
+        def map_func(input):
+            new_input: Dict = {k: v for k, v in inputs.items() if k not in self.map_keys}
+            for mapping_key in self.map_keys:
+                new_input[mapping_key] = input[mapping_key]
             result = self.chain(**new_input)
-            return {"index":text_dict["index"], "result":result}
-        results = pool.map(map_func, text_dicts)
+            return {"index":input["index"], "result":result}
+        results = pool.map(map_func, inputs_list)
         pool.close()
 
         # 重新依据 self.rank_key 的值进行排序，默认是原有顺序。
--- uglygpt/chains/reduce.py
+++ uglygpt/chains/reduce.py
@@ -12,52 +12,30 @@ from .llm import LLMChain
 @dataclass
 class ReduceChain(Chain):
     chain: Chain = field(default_factory=LLMChain)
-    reduce_key: str = "input"
-    separator: str = "\n"
-    check_func: Optional[Callable[[str], bool]] = None # 默认不分组进行reduce，如果需要分组（大概率是因为字数限制），需要传入一个函数，根据字符串返回True/False
+    reduce_keys: List[str] = field(default_factory=lambda: ["input"])
+    format: Callable[[str], str] = field(default_factory=lambda: lambda x: x)
 
     @property
     def input_keys(self) -> List[str]:
         return self.chain.input_keys
 
     def _validate_inputs(self, inputs: Dict[str, Any]) -> None:
-        if self.reduce_key not in self.input_keys:
-            raise ValueError(
-                f"MapChain expects {self.reduce_key} to be in input_keys")
-        if self.check_func is not None:
-            if "history" not in self.input_keys:
-                raise ValueError(
-                    f"MapChain expects history to be in input_keys")
-            inputs["history"] = ""
+        self.num = len(inputs[self.reduce_keys[0]])
+        for reduce_key in self.reduce_keys:
+            assert reduce_key in self.input_keys, f"MapChain expects {reduce_key} to be in input_keys"
+            assert isinstance(inputs[reduce_key], List), f"MapChain expects {reduce_key} to be a list of strings"
+            assert len(inputs[reduce_key]) == self.num, f"MapChain expects {reduce_key} to be a list of strings with the same length"
+        assert "history" in self.input_keys, f"MapChain expects history to be in input_keys"
+        inputs["history"] = ""
         super()._validate_inputs(inputs)
-        if not isinstance(inputs[self.reduce_key], List):
-            raise ValueError(
-                f"MapChain expects {self.reduce_key} to be a list of strings")
 
     def _call(self, inputs: Dict[str, Any]) -> str:
-        # combine the inputs
-        text_list = []
-        text = ""
-        for item in inputs[self.reduce_key]:
-            if self._is_too_long(text + self.separator + item):
-                text_list.append(text)
-                text = item
-            else:
-                text +=  self.separator + item if text!= "" else item
-        text_list.append(text)
-        #logger.debug(f"text_list: {text_list}")
-
         result = ""
-        for text in text_list:
-            new_input: Dict = {k: v for k, v in inputs.items() if k != self.reduce_key}
-            new_input["history"] = result
-            new_input[self.reduce_key] = text
-            #logger.debug(f"new_input: {new_input}")
+        for i in range(self.num):
+            new_input: Dict = {k: v for k, v in inputs.items() if k not in self.reduce_keys}
+            new_input["history"] = self.format(result)
+            for reduce_key in self.reduce_keys:
+                new_input[reduce_key] = inputs[reduce_key][i]
             result = self.chain(**new_input)
             logger.debug(f"result: {result}")
         return result
-
-    def _is_too_long(self, text: str) -> bool:
-        if self.check_func is None:
-            self.check_func = lambda x: False
-        return True if self.check_func(text) else False
