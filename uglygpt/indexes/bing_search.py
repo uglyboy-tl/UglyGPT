65680ea2a254bc8f3bc99535aca6add5e7b44afd
uglyboy-tl <25730357+uglyboy-tl@users.noreply.github.com>
2023-09-26 13:26:48 +0800

    增加 DDGS 搜索（被墙了），增加网络超时判断和异常处理

--- uglygpt/indexes/bing_search.py
+++ uglygpt/indexes/bing_search.py
@@ -3,7 +3,7 @@
 
 import requests
 from loguru import logger
-from tenacity import retry, stop_after_attempt, wait_fixed
+from tenacity import retry, stop_after_attempt, wait_fixed, before_sleep_log, RetryError
 from dataclasses import dataclass
 
 from uglygpt.base import config
@@ -18,25 +18,26 @@ class BingSearch(Index):
         assert isinstance(config.bing_subscription_key, str), 'Bing subscription key should be a string'
         assert config.bing_subscription_key, 'Bing subscription key should not be empty'
 
-    @retry(stop=stop_after_attempt(3), wait=wait_fixed(1))
+    def search(self, query: str, n: int = 5) -> list[str]:
+        try:
+            response = self._send_request(query, n)
+            json = response.json()
+            results = json.get('webPages', {}).get('value', [])
+            return [self._format(result) for result in results[:n]] if results else []
+        except (ValueError, requests.RequestException, RetryError) as e:
+            logger.error(f'Error occurred while sending request or parsing response: {e}')
+            return []
+
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(1), before_sleep=before_sleep_log(logger, "WARNING")) # type: ignore
     def _send_request(self, query: str, n: int):
         response = requests.get(
             BING_SEARCH_API_URL,
             headers={'Ocp-Apim-Subscription-Key': config.bing_subscription_key},
-            params={'q': query, 'count': n}
+            params={'q': query, 'count': n},
+            timeout=5
         )
         response.raise_for_status()
         return response
-
-    def search(self, query: str, n: int) -> list[str]:
-        response = self._send_request(query, n)
-        try:
-            json = response.json()
-            results = json.get('webPages', {}).get('value', [])
-            return [self._format(result) for result in results[:n]] if results else []
-        except requests.exceptions.RequestException as e:
-            logger.error(f'Error occurred while sending request to Bing: {e}')
-            raise e
 
     @classmethod
     def _format(cls, result: dict) -> str:
---
+++ uglygpt/indexes/duckduckgo_search.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8-*-
+
+import requests
+from loguru import logger
+from tenacity import retry, stop_after_attempt, wait_fixed, before_sleep_log, RetryError
+from dataclasses import dataclass
+
+from .base import Index
+
+DUCKDUCKGO_SEARCH_API_URL = "https://api.duckduckgo.com"
+
+@dataclass
+class DuckDuckGo(Index):
+    def search(self, query: str, n: int = 5) -> list[str]:
+        try:
+            response = self._send_request(query)
+            json = response.json()
+            results = json.get("Results", [])
+            return [self._format(result) for result in results[:n]] if results else []
+        except (ValueError, requests.RequestException, RetryError) as e:
+            logger.error(f'Error occurred while sending request or parsing response: {e}')
+            return []
+
+    @retry(stop=stop_after_attempt(3), wait=wait_fixed(1), before_sleep=before_sleep_log(logger, "WARNING")) # type: ignore
+    def _send_request(self, query: str):
+        params = {
+            "q": query,
+            "format": "json",
+            "pretty": 1,
+        }
+        response = requests.get(DUCKDUCKGO_SEARCH_API_URL, params=params, timeout=5)
+        response.raise_for_status()
+        return response
+
+    @classmethod
+    def _format(cls, result: dict) -> str:
+        title = result.get('Title', '')
+        url = result.get('FirstURL', '')
+        snippet = result.get('Text', '')
+        if not all([title, url, snippet]):
+            logger.error(f'Missing key in search result: {result}')
+            raise KeyError('Missing key in search result')
+        return f"[{title}]({url})\n> {snippet}"
