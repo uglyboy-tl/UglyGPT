4ce114ddc253eda4efa555ba2205b0bc49fafb82
uglyboy-tl <25730357+uglyboy-tl@users.noreply.github.com>
2023-09-26 15:31:09 +0800

    增加 Index 包对外露出的各类搜索引擎

--- uglygpt/indexes/__init__.py
+++ uglygpt/indexes/__init__.py
@@ -1,6 +1,14 @@
 from .base import Index, Memory
+from .bing_search import BingSearch
+from .arxiv import ArxivIndex
+from .wiki import WikipediaSearch
+from .duckduckgo_search import DuckDuckGo
 
 __all__ = [
     'Index',
     'Memory',
+    'BingSearch',
+    'ArxivIndex',
+    'WikipediaSearch',
+    'DuckDuckGo'
 ]
