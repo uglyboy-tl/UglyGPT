4c51df2e3cd8a9f0d2b2ad0390d992efeab00764
uglyboy-tl <25730357+uglyboy-tl@users.noreply.github.com>
2023-09-29 22:14:37 +0800

    包中增加 bm25

--- uglygpt/indexes/__init__.py
+++ uglygpt/indexes/__init__.py
@@ -3,6 +3,7 @@ from .bing_search import BingSearch
 from .arxiv import ArxivIndex
 from .wiki import WikipediaSearch
 from .duckduckgo_search import DuckDuckGo
+from .bm25 import BM25
 
 __all__ = [
     'Index',
@@ -10,5 +11,6 @@ __all__ = [
     'BingSearch',
     'ArxivIndex',
     'WikipediaSearch',
-    'DuckDuckGo'
+    'DuckDuckGo',
+    'BM25',
 ]
