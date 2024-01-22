6d57e57e337918a7b3c47e8b1a81de0a5b0b947f
uglyboy-tl <25730357+uglyboy-tl@users.noreply.github.com>
2024-01-22 20:36:14 +0800

    change gpt name

--- core/llm/model.py
+++ core/llm/model.py
@@ -2,12 +2,12 @@ from enum import Enum
 from core.base import config
 
 class Model(Enum):
-    GPT_TURBO = "gpt-3.5-turbo"
-    GPT_TURBO_16K = "gpt-3.5-turbo-16k"
+    GPT3_TURBO = "gpt-3.5-turbo"
+    GPT3_TURBO_16K = "gpt-3.5-turbo-16k"
     GPT4 = "gpt-4"
     GPT4_32K = "gpt-4-32k"
     GPT4_TURBO = "gpt-4-turbo"
-    COPILOT_TURBO = "copilot-3.5"
+    COPILOT3_TURBO = "copilot-3.5"
     COPILOT4 = "copilot-4"
     YI = "yi"
     YI_32K = "yi-32k"
