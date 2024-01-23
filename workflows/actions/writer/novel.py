03e1ef2b11958452823c164296a9276daf7b984e
uglyboy-tl <25730357+uglyboy-tl@users.noreply.github.com>
2024-01-23 19:28:09 +0800

    use instructor to novel

--- workflows/actions/writer/novel.py
+++ workflows/actions/writer/novel.py
@@ -2,12 +2,12 @@
 # -*-coding:utf-8-*-
 
 from dataclasses import dataclass, field
+from typing import Optional
 
-from loguru import logger
+from pydantic import BaseModel, Field
 
-from core import ReduceChain
+from core import ReduceChain, Model
 from agent.retrievers import StoresRetriever, get_stores_retriever
-from workflows.utils import parse_json
 from ..base import Action
 
 ROLE="""
@@ -16,12 +16,21 @@ ROLE="""
 
 输出段落：小说的下一段。输出的段落应该包含大约20个句子，并且应该遵循输入的指示。
 输出记忆：更新后的记忆。你应该首先解释输入记忆中哪些句子不再需要以及为什么，然后解释需要添加什么到记忆中以及为什么。然后你应该写更新后的记忆。更新后的记忆应该与输入的记忆类似，除了你之前认为应该被删除或添加的部分。更新后的记忆只应储存关键信息。更新后的记忆绝不能超过20个句子！
-
+注意除格式要求的字段名外，其他内容请使用中文进行文本输出。
+"""
+"""
 请按照以下示例格式直接返回 JSON 结果，其中 output_paragraph 为输出段落，Rational 为记忆更新的理由，Updated Memory 是更新后的记忆。请确保你返回的结果可以被 Python json.loads 解析。如果你的目标已经完成或无法解决，则不需要给出命令行指令。
 
 格式示例：
 {{"output_paragraph": "{{string of output paragraph, around 20 sentences.}}","output_memory": {{"Rational":"{{string that explain how to update the memory}}","Updated Memory": "{{string of updated memory, around 10 to 20 sentences}}"}} }}
 """
+class OutputMemory(BaseModel):
+    rational: str = Field(..., description="string that explain how to update the memory")
+    updated_memory: str= Field(..., description="string of updated memory, around 10 to 20 sentences")
+
+class NovelDetail(BaseModel):
+    output_paragraph: str = Field(..., description="string of output paragraph, around 20 sentences.")
+    output_memory: OutputMemory
 
 PROMPT_TEMPLATE = """
 这些是输入：
@@ -31,33 +40,28 @@ PROMPT_TEMPLATE = """
 {input}
 
 -----
-现在开始写作，严格按照格式示例中的输出格式组织你的输出。
-
-非常重要！更新后的记忆只应储存关键信息。更新后的记忆绝不能超过500个单词！
-
-非常重要：
-你应该首先解释输入记忆中哪些句子不再需要以及为什么，然后解释需要添加什么到记忆中以及为什么。然后，你开始重写输入记忆以获得更新后的记忆。
 """
 
 @dataclass
 class Novel(Action):
     filename: str = "docs/examples/novel.txt"
-    llm_name: str = "gpt-3.5-turbo-16k"
+    model: Model = Model.GPT3_TURBO_16K
     prompt: str = PROMPT_TEMPLATE
     role: str = ROLE
+    response_model: Optional[type[BaseModel]] = NovelDetail
     reduce: ReduceChain = field(init=False)
     db: StoresRetriever = field(init=False)
 
     def __post_init__(self):
-        self.llm = ReduceChain(self.prompt, self.llm_name, self.role, format=self._parse)
+        self.llm = ReduceChain(self.prompt, self.model, self.role, self.response_model, format=self._parse)
         self.db = get_stores_retriever("bm25", "docs/examples/novel.json", True)
         self.db.init()
         return super().__post_init__()
 
     def _parse(self, response:str) -> str:
-        data = parse_json(response)
-        short_memory = data.get("output_memory").get("Updated Memory", "")
-        input_paragraph = data.get("output_paragraph", "")
+        data: NovelDetail = self.llm.llm.parse_response(response, NovelDetail) # type: ignore
+        input_paragraph = data.output_paragraph
+        short_memory = data.output_memory.rational
         long_memory = self.db.search(input_paragraph, 3)
         input_long_term_memory = '\n'.join(
             [f"相关段落 {i+1} :" + selected_memory for i, selected_memory in enumerate(long_memory)])
