817325be147fc1b933cd4f4d4bd1f6e33a577e61
uglyboy-tl <25730357+uglyboy-tl@users.noreply.github.com>
2024-01-26 08:38:14 +0800

    🔧 修改(.gitignore)：删除docs/md文件夹 🔧 修改(github_api.py)：将文件路径从docs/md/trending.md更新为resource/md/trending.md

--- workflows/utils/github_api.py
+++ workflows/utils/github_api.py
@@ -64,7 +64,7 @@ class GithubAPI:
 
     @classmethod
     def fetch_trending_file(cls) -> str:
-        file_name = "docs/md/trending.md"
+        file_name = "resource/md/trending.md"
         if File.datetime(file_name).date() < datetime.today().date():
             date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
             try:
